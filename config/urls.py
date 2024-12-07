68c1fe3a544e7dfcc2b6067de50065b9c96ba0a1
Alryum <alryum@yandex.ru>
2024-12-07 20:33:21 +0500

    include tickets app urls

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,4 +18,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('tickets.urls')),
 ]
