6f9e77d7cad4af888e5debdc4ada48511fecbb43
Alryum <alryum@yandex.ru>
2024-12-07 23:56:41 +0500

    refactor

--- tickets/models.py
+++ tickets/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Ticket(models.Model):
     STATUS_CHOICES = [
         ('new', 'New'),
         ('in_progress', 'In Progress'),
         ('closed', 'Closed'),
     ]
-    
+
     subject = models.CharField(max_length=255)
     user_email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
-    operator = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='tickets')
+    operator = models.ForeignKey(User, null=True, blank=True,
+                                 on_delete=models.SET_NULL, related_name='tickets')
 
     def __str__(self):
         return f'Обращение {self.id} - {self.subject}'
+
 
 class Message(models.Model):
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name="messages")
